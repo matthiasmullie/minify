c7dcdc4934c6721b2b683f5d207a31b61da1ef44
Matthias Mullie <git@mullie.eu>
2015-08-20 16:32:14 +0200

    Add test for issue #64

--- tests/js/JSTest.php
+++ tests/js/JSTest.php
@@ -623,6 +623,18 @@ BUG
             'isPath:function(e){return/\//.test(e)}',
         );
 
+        // https://github.com/matthiasmullie/minify/issues/64
+        $tests[] = array(
+            '    var d3_nsPrefix = {
+        svg: "http://www.w3.org/2000/svg",
+        xhtml: "http://www.w3.org/1999/xhtml",
+        xlink: "http://www.w3.org/1999/xlink",
+        xml: "http://www.w3.org/XML/1998/namespace",
+        xmlns: "http://www.w3.org/2000/xmlns/"
+    };',
+            'var d3_nsPrefix={svg:"http://www.w3.org/2000/svg",xhtml:"http://www.w3.org/1999/xhtml",xlink:"http://www.w3.org/1999/xlink",xml:"http://www.w3.org/XML/1998/namespace",xmlns:"http://www.w3.org/2000/xmlns/"}',
+        );
+
         return $tests;
     }
 }
