matthiasmullie/minify

commit 04a37f5434c618cae16a7dca13592a567bc36246

Make sure $positions & $matches are in sync
Fixes #238

src/Minify.php
@@ -258,7 +258,7 @@ abstract class Minify
                     // executing it again in later runs on this same content;
                     // ignore this one until we reach end of content
                     unset($matches[$i]);
-                    $positions[$i] = strlen($content);
+                    unset($positions[$i]);
                 }
             }
 

tests/js/JSTest.php
@@ -1270,8 +1270,8 @@ var largeScreen=2048',
 
         // https://github.com/matthiasmullie/minify/issues/242
         $tests[] = array(
-            "/^\[(x| )\](?=\s)/i // Must follow listRE",
-            "/^\[(x| )\](?=\s)/i",
+            '/^\[(x| )\](?=\s)/i // Must follow listRE',
+            '/^\[(x| )\](?=\s)/i',
         );
 
         // known minified files to help doublecheck changes in places not yet

commit 0a6dc95895d6248fe1dfb8d83a9f570bb589d2a8

Correcting "Undefined offset: 0"
https://github.com/matthiasmullie/minify/commit/04a37f5434c618cae16a7dca13592a567bc36246 caused a regression by unsetting $positions followed by an array index read which fails with "Undefined offset: 0"

src/Minify.php
@@ -241,7 +241,7 @@ abstract class Minify
 
                 // no need to re-run matches that are still in the part of the
                 // content that hasn't been processed
-                if ($positions[$i] >= 0) {
+                if (isset($positions[$i])  &&  $positions[$i] >= 0) {
                     continue;
                 }
 